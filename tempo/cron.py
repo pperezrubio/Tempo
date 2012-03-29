2209e47d2a65dee16a3a15446fbc5c55f757a43e
Rick Harris <rconradharris@gmail.com>
2012-03-29 22:34:57 +0000

    Adding schedule override config for testing.

--- tempo/cron.py
+++ tempo/cron.py
@@ -15,13 +15,31 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 import logging
+import os
 
+from tempo import config
 from tempo import db
+from tempo.openstack.common import cfg
 from tempo.openstack.common import exception as common_exception
 from tempo.openstack.common import utils as common_utils
 
 logger = logging.getLogger('tempo.cron')
 
+CFG = config.CFG
+
+cron_opts = [
+    cfg.StrOpt('schedule_override',
+               default=None,
+               help='Cron formatted schedule to use for all tasks.'
+                    ' For debugging purposes.'),
+    cfg.StrOpt('tempo_enqueue_path',
+               default='/usr/local/bin',
+               help='Path to tempo-enqueue.')
+]
+
+cron_group = cfg.OptGroup(name='cron', title='Tempo Cron options')
+CFG.register_group(cron_group)
+CFG.register_opts(cron_opts, group=cron_group)
 
 def crontab():
     DAY_OF_MONTH = "*"
@@ -29,10 +47,15 @@ def crontab():
 
     lines = []
     for task in db.task_get_all():
-        minute, hour, day_of_week = task.cron_schedule.split(' ')
-        schedule = ' '.join([minute, hour, DAY_OF_MONTH, MONTH, day_of_week])
-        line = '%s tempo-enqueue %s' % (schedule, task.uuid)
-        lines.append(line)
+
+        if CFG.cron.schedule_override:
+            schedule = CFG.cron.schedule_override
+        else:
+            minute, hour, day_of_week = task.cron_schedule.split(' ')
+            schedule = ' '.join([minute, hour, DAY_OF_MONTH, MONTH, day_of_week])
+
+        bin_path = os.path.join(CFG.cron.tempo_enqueue_path, 'tempo-enqueue')
+        lines.append(' '.join([schedule, bin_path, task.uuid]))
 
     # Trailing new line is required by cron format
     lines.append('')
